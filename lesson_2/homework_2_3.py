0790445749b631ecc5ce621f6fce121daf00021c
natcupo <natalycupo@outlook.com>
2020-04-17 16:22:18 -0400

    Temperature

--- lesson_2/homework_2_3.py
+++ lesson_2/homework_2_3.py
@@ -2,6 +2,10 @@
 # a string (e.g. '75') and convert it to Celsius.
 # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
 
-# type your code here
-temperature_fahrenheit = None
-result_temperature = None
+print('Hello this is my first program. It will convert Fahrenheit into Celsius')
+temperature = input("What is your current temperature in celsius")
+temperature_fahrenheit = '78'
+result_temperature = (int(temperature_fahrenheit) - 32) * 5/9
+result_temperature_celsius = round(result_temperature, 0)
+print(result_temperature_celsius)
+print(f'Well, celsius temperature for 78 degrees is {result_temperature_celsius}')
